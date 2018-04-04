3f1fc51bd109c20c7db849ba95881bc8838afbba
J. Beckett Sweeney <jbeckettsweeney@gmail.com>
2018-04-04 15:52:33 -0600

    reads from input.csv

--- src/graphalgorithms/GraphAlgorithms.java
+++ src/graphalgorithms/GraphAlgorithms.java
@@ -1,4 +1,10 @@
 package graphalgorithms;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 /*
  * Authors: Bailey Kouwenhoven and J. Beckett Sweeney
  * Date: 
@@ -6,8 +12,28 @@ package graphalgorithms;
  */
 public class GraphAlgorithms {
     public static void main(String[] args) {
-        // this is gonna be neat
-        System.out.println("did this work?");
+        String csvFile = "input.csv";
+        BufferedReader br = null;
+        String line = "";
+        
+        try {
+            br = new BufferedReader(new FileReader(csvFile));
+            while((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch(FileNotFoundException e) {
+            e.printStackTrace();
+        } catch(IOException e) {
+            e.printStackTrace();
+        } finally {
+            if(br != null) {
+                try {
+                    br.close();
+                } catch(IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
     
 }
